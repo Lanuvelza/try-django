ff616a6708d44328bc8910502513166e0e3866e3
Ian <ianlam1221@hotmail.com>
2021-07-12 15:08:35 -0700

    passed dummy context variablesto templates in home

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+  {
+     'author': 'CoreyMS', 
+     'title': 'Blog Post 1', 
+     'content': 'First post conetent', 
+     'date_posted': 'August 27, 2018'
+  },
+  {
+     'author': 'Jane Doe', 
+     'title': 'Blog Post 2', 
+     'content': 'Second post conetent', 
+     'date_posted': 'August 28, 2018'
+  }
+]
 
 # Create your views here.
 
 def home(request):
-  return render(request, 'blog/home.html')
+  context = {
+    'posts': posts
+  }
+  return render(request, 'blog/home.html', context)
 
 def about(request):
   return render(request, 'blog/about.html')
